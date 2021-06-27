f14874c33346aa75d6ccc9d7fb8f516c2f9c1f7a
Marcos Volpi <marcos.volpi@BP4.com>
2021-06-26 21:31:20 -0300

    Add test

--- tests/soupTest.js
+++ tests/soupTest.js
@@ -11,6 +11,13 @@ describe("soup", function() {
       expect(res).to.equal(2);
     });
 
+    it("should return 3 when search words in soup", async function() {
+      const soupOb = new soup();
+      const res=soupOb.searchInSoup("OIE,IIX,EXE", 3, 3);
+      
+      expect(res).to.equal(3);
+    });    
+
     it("should return true when try to validate columns", async function() {
       const soupOb = new soup();
       const res=soupOb.verifyColumns(3, "EIO,EIO");
