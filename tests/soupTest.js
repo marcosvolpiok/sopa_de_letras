1ca772f3014b759f2aec2f7f0670164951988be2
Marcos Volpi <marcos.volpi@BP4.com>
2021-06-26 21:28:27 -0300

    Add test

--- tests/soupTest.js
+++ tests/soupTest.js
@@ -4,17 +4,31 @@ const soup = require('../soup');
 
 
 describe("soup", function() {
-    it("should return 2", async function() {
+    it("should return 2 when search words in soup", async function() {
       const soupOb = new soup();
       const res=soupOb.searchInSoup("EIO,EIO", 2, 3);
       
       expect(res).to.equal(2);
     });
 
-    it("should return true", async function() {
+    it("should return true when try to validate columns", async function() {
       const soupOb = new soup();
       const res=soupOb.verifyColumns(3, "EIO,EIO");
       
       expect(res).to.equal(true);
     });
+
+    it("should return false when try to validate columns", async function() {
+      const soupOb = new soup();
+      const res=soupOb.verifyColumns(3, "EIO,EIOxxxx");
+      
+      expect(res).to.equal(false);
+    });
+
+    it("should return true when try to validate rows", async function() {
+      const soupOb = new soup();
+      const res=soupOb.verifyRows(2, "EIO,EIO");
+      
+      expect(res).to.equal(true);
+    });    
 });
